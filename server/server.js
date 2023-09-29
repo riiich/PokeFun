1a6fa32b746975f3d9f0042fbe89a456cd6c138d
Rich <horich21@gmail.com>
2023-09-29 06:43:05 -0700

    added an environment var

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3001;
 const cors = require('cors');
+const port = process.env.PORT || 3001;
 
 //  middleware that parses incoming requests with JSON payloads and is based on body-parser
 app.use(express.json());     //express.json is ONLY used for POST and PUT requests, not GET and DELETE requests
